7587ec94c2e9122be10fa93ce8a5c1f2c9997ad5
RabbieW <45704802+RabbieW@users.noreply.github.com>
2018-12-25 22:34:54 +0800

    Update gzoj5-2-6.cpp
    你想复杂了......

--- gzoj5-2-6.cpp
+++ gzoj5-2-6.cpp
@@ -24,3 +24,21 @@ int main()
 	printf("%d",tmp);
 	return 0;
 } 
+/*
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int i,a1,a2,n,a,b,x=0;
+	cin >> n >> a1 >> a2;
+	for (i = 1;i <= n;i++)
+	{
+		if (i % a1 == 0) a = a1;
+		else a = i % a1;
+		if (i % a2 == 0) b = a2;
+		else b = i % a2;
+		if (a == b) x++;
+	}
+	cout << x;
+}
+*/
